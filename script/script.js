e4eff2d8808cca13e29b4ff8daacdd83afe58199
atiqur-rahman1992 <atiqurrahman1045man@gmail.com>
2025-03-08 01:50:15 +0600

    Final edit

--- script/script.js
+++ script/script.js
@@ -1,4 +1,27 @@
-document.getElementById('button-01').addEventListener('click',function(){
+document.getElementById('random').addEventListener('click',function(){
+    const button = document.getElementById("random");
+
+    function getRandomColor() {
+      return "#" + Math.floor(Math.random() * 16777215).toString(16);
+    }
+  
+    button.addEventListener("click", function() {
+      document.body.style.backgroundColor = getRandomColor();
+    });
+
+})
+
+
+
+
+
+const time = document.getElementById('h2-id').innerText;
+
+
+  let now = new Date();
+  document.getElementById('h2-id').innerText = now;
+
+  document.getElementById('button-01').addEventListener('click',function(){
     alert('board upated successfully');
     ////code for box1
    const h1Value =  document.getElementById('first-h1').innerText;
@@ -28,3 +51,192 @@ document.getElementById('button-01').addEventListener('click',function(){
 
 
 })
+document.getElementById('clear-btn').addEventListener('click',function(){
+    console.log('btn click')
+    document.getElementById('add-text').style.display = 'none';
+})
+
+
+// button -02
+document.getElementById('button-02').addEventListener('click',function(){
+    alert('board upated successfully');
+    ////code for box1
+   const h1Value =  document.getElementById('first-h1').innerText;
+   const ch1Value = parseInt(h1Value)
+  const upValue = ch1Value + 1;
+  console.log(upValue);
+  document.getElementById('first-h1').innerText = upValue
+  const subValue =document.getElementById('sub-value').innerText;
+  console.log(subValue);
+  const csubValue = parseInt(subValue);
+  const upValue1 = csubValue -1;
+  document.getElementById('sub-value').innerText = upValue1;
+  let button = document.getElementById('button-02');
+  button.disabled = true;
+  let now = new Date();
+  
+  const container = document.getElementById('add-text')
+  const div = document.createElement('div');
+  div.classList.add('bg-orange-200')
+  div.innerHTML= `
+  <p>You have Complete The Task Add Dark Mode at ${now} PMco</p>
+  
+  `
+  container.appendChild(div);
+
+  
+
+
+})
+document.getElementById('clear-btn').addEventListener('click',function(){
+    console.log('btn click')
+    document.getElementById('add-text').style.display = 'none';
+})
+
+//button 3
+document.getElementById('button-03').addEventListener('click',function(){
+    alert('board upated successfully');
+    ////code for box1
+   const h1Value =  document.getElementById('first-h1').innerText;
+   const ch1Value = parseInt(h1Value)
+  const upValue = ch1Value + 1;
+  console.log(upValue);
+  document.getElementById('first-h1').innerText = upValue
+  const subValue =document.getElementById('sub-value').innerText;
+  console.log(subValue);
+  const csubValue = parseInt(subValue);
+  const upValue1 = csubValue -1;
+  document.getElementById('sub-value').innerText = upValue1;
+  let button = document.getElementById('button-03');
+  button.disabled = true;
+  let now = new Date();
+  
+  const container = document.getElementById('add-text')
+  const div = document.createElement('div');
+  div.classList.add('bg-orange-200')
+  div.innerHTML= `
+  <p>You have Complete The Task Add Dark Mode at ${now} PMco</p>
+  
+  `
+  container.appendChild(div);
+
+  
+
+
+})
+document.getElementById('clear-btn').addEventListener('click',function(){
+    console.log('btn click')
+    document.getElementById('add-text').style.display = 'none';
+})
+/// button 04
+document.getElementById('button-04').addEventListener('click',function(){
+    alert('board upated successfully');
+    ////code for box1
+   const h1Value =  document.getElementById('first-h1').innerText;
+   const ch1Value = parseInt(h1Value)
+  const upValue = ch1Value + 1;
+  console.log(upValue);
+  document.getElementById('first-h1').innerText = upValue
+  const subValue =document.getElementById('sub-value').innerText;
+  console.log(subValue);
+  const csubValue = parseInt(subValue);
+  const upValue1 = csubValue -1;
+  document.getElementById('sub-value').innerText = upValue1;
+  let button = document.getElementById('button-04');
+  button.disabled = true;
+  let now = new Date();
+  
+  const container = document.getElementById('add-text')
+  const div = document.createElement('div');
+  div.classList.add('bg-orange-200')
+  div.innerHTML= `
+  <p>You have Complete The Task Add Dark Mode at ${now} PMco</p>
+  
+  `
+  container.appendChild(div);
+
+  
+
+
+})
+document.getElementById('clear-btn').addEventListener('click',function(){
+    console.log('btn click')
+    document.getElementById('add-text').style.display = 'none';
+})
+//button-05
+document.getElementById('button-05').addEventListener('click',function(){
+    alert('board upated successfully');
+    ////code for box1
+   const h1Value =  document.getElementById('first-h1').innerText;
+   const ch1Value = parseInt(h1Value)
+  const upValue = ch1Value + 1;
+  console.log(upValue);
+  document.getElementById('first-h1').innerText = upValue
+  const subValue =document.getElementById('sub-value').innerText;
+  console.log(subValue);
+  const csubValue = parseInt(subValue);
+  const upValue1 = csubValue -1;
+  document.getElementById('sub-value').innerText = upValue1;
+  let button = document.getElementById('button-05');
+  button.disabled = true;
+  let now = new Date();
+  
+  const container = document.getElementById('add-text')
+  const div = document.createElement('div');
+  div.classList.add('bg-orange-200')
+  div.innerHTML= `
+  <p>You have Complete The Task Add Dark Mode at ${now} PMco</p>
+  
+  `
+  container.appendChild(div);
+
+  
+
+
+})
+document.getElementById('clear-btn').addEventListener('click',function(){
+    console.log('btn click')
+    document.getElementById('add-text').style.display = 'none';
+})
+///button-06
+document.getElementById('button-06').addEventListener('click',function(){
+    alert('board upated successfully');
+    alert('hellow world')
+    ////code for box1
+   const h1Value =  document.getElementById('first-h1').innerText;
+   const ch1Value = parseInt(h1Value)
+  const upValue = ch1Value + 1;
+  console.log(upValue);
+  document.getElementById('first-h1').innerText = upValue
+  const subValue =document.getElementById('sub-value').innerText;
+  console.log(subValue);
+  const csubValue = parseInt(subValue);
+  const upValue1 = csubValue -1;
+  document.getElementById('sub-value').innerText = upValue1;
+  let button = document.getElementById('button-06');
+  button.disabled = true;
+  let now = new Date();
+  
+  const container = document.getElementById('add-text')
+  const div = document.createElement('div');
+  div.classList.add('bg-orange-200 text-[2px]')
+  div.innerHTML= `
+  <p class="text-[2px]">You have Complete The Task Add Dark Mode at ${now} PMco</p>
+  
+  `
+  container.appendChild(div);
+
+  
+
+
+})
+document.getElementById('clear-btn').addEventListener('click',function(){
+    console.log('btn click')
+    document.getElementById('add-text').style.display = 'none';
+})
+////change page
+document.getElementById('change-page').addEventListener('click',function(){
+    window.location.href = 'change.html'
+})
+
+
